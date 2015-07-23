67c06f3e0185fb40efce7b6f0f5f4e2c08a34eb0
Joe Sutherland <1004421@rgu.ac.uk>
2015-07-23 20:28:24 +0100

    First

---
+++ getURLTitle.php
@@ -0,0 +1,10 @@
+function get_page_title($url){
+
+	if( !($data = file_get_contents($url)) ) return false;
+
+	if( preg_match("#(.+)<\/title>#iU", $data, $t))  {
+		return trim($t[1]);
+	} else {
+		return false;
+	}
+}
---
+++ script.js
@@ -0,0 +1,50 @@
+function generateEmail(){
+var url = urlInput.value;
+var author = authorInput.value;
+var twitter = twitterInput.value;
+var facebook = FBInput.value;
+
+$.ajax({
+  type: "POST",
+  url: "./getURLTitle.php",
+  data: "{url: \"" + url + "\"}",
+  success: function(data) {
+     var title = data;
+     alert(data);
+  }   
+});
+
+result.innerHTML = "Hey all,<br/><br />We just published \""+title+"\" to the blog. URL:<br /><br />"+url+"<br /><br />Many thanks to "+author+" for this post.<br /><br />Below are some proposed social media messages. Please tweak as needed.<br /><br /><b>Twitter (@wikipedia/@wikimedia):</b><br />• "+twitter+" "+url+"<br /><br /><b>Facebook/Google+:</b><br />• "+facebook+" "+url+"<br/ ><br />thanks,<br />Joe";
+
+};
+
+function SelectText(element) { //this code from https://stackoverflow.com/questions/985272/selecting-text-in-an-element-akin-to-highlighting-with-your-mouse
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+    try {
+
+		// The important part (copy selected text)
+		var successful = document.execCommand('copy');
+
+		// "Optional" remove selected text
+		sel.removeAllRanges();
+
+		if(successful) answer.innerHTML = 'Copied!';
+		else answer.innerHTML = 'Unable to copy; please use ctrl+c!';
+		} catch (err) {
+		answer.innerHTML = 'Unsupported browser; please use ctrl+c!';
+	}
+};
